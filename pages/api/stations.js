7148599981bee952f946c4901757ff06ee68942f
ParkJunSeok <pjh203203@naver.com>
2025-05-30 11:18:43 +0900

    Move csv to public/data and update path

--- pages/api/stations.js
+++ pages/api/stations.js
@@ -4,10 +4,14 @@ import { parse } from "csv-parse/sync";
 
 export default function handler(req, res) {
   try {
-    const filePath = path.join(process.cwd(), "data", "bus_station_data.csv");
+    const filePath = path.join(
+      process.cwd(),
+      "public",
+      "data",
+      "bus_station_data.csv"
+    );
     const fileContent = fs.readFileSync(filePath, "utf-8");
     const records = parse(fileContent, {
-
       columns: (header) => header.map((h) => h.replace(/\uFEFF/g, "")),
       skip_empty_lines: true,
     });
